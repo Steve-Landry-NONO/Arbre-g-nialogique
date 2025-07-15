b53698c770a3a428e2b422fac6a8289c4c0c8408
steve-landry.kouokam-nono <steve-landry.kouokam-nono@ens.uvsq.fr>
2025-07-15 14:10:51 +0200

    ajout de la classe Mariage et gestion des unions dans la classe Famille (polygamie incluse)

--- arbre_genealogique.py
+++ arbre_genealogique.py
@@ -40,10 +40,35 @@ class Personne:
         p.enfants = data.get("enfants", [])
         return p
 
+class Mariage:
+    def __init__(self, id_mariage, conjoint_1, conjoint_2, enfants=None):
+        self.id = id_mariage
+        self.conjoint_1 = conjoint_1  # ID Personne
+        self.conjoint_2 = conjoint_2  # ID Personne
+        self.enfants = enfants if enfants else []  # Liste d’IDs Personne
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "conjoint_1": self.conjoint_1,
+            "conjoint_2": self.conjoint_2,
+            "enfants": self.enfants
+        }
+
+    @staticmethod
+    def from_dict(data):
+        return Mariage(
+            id_mariage=data["id"],
+            conjoint_1=data["conjoint_1"],
+            conjoint_2=data["conjoint_2"],
+            enfants=data.get("enfants", [])
+        )
+
 
 class Famille:
     def __init__(self):
         self.membres = {}  # id: Personne
+        self.mariages = {}  # id: Mariage
 
     def ajouter_personne(self, personne):
         self.membres[personne.id] = personne
@@ -54,6 +79,19 @@ class Famille:
             for p_data in data:
                 p = Personne.from_dict(p_data)
                 self.ajouter_personne(p)
+
+    def charger_mariages_depuis_json(self, chemin_fichier):
+        with open(chemin_fichier, 'r', encoding='utf-8') as f:
+            data = json.load(f)
+            for m_data in data:
+                mariage = Mariage.from_dict(m_data)
+                self.mariages[mariage.id] = mariage
+
+    def ajouter_mariage(self, mariage):
+        self.mariages[mariage.id] = mariage
+
+    def get_mariages_de(self, personne_id):
+        return [m for m in self.mariages.values() if m.conjoint_1 == personne_id or m.conjoint_2 == personne_id]
 
     def sauvegarder_vers_json(self, chemin_fichier):
         data = [p.to_dict() for p in self.membres.values()]
@@ -76,6 +114,7 @@ class Famille:
 if __name__ == "__main__":
     famille = Famille()
     famille.charger_depuis_json("famille.json")
+    famille.charger_mariages_depuis_json("mariages.json")
     famille.afficher_arbre()
     # Pour ajouter une personne :
     # nouvelle = Personne("Nouveau", "Nom")
